19604b06610d1db60c9936b9f12112b854f77d63
yukisae <th.marisa@gmail.com>
2025-03-13 14:45:47 +0000

    Improve logging performance

--- linux/bash/logging/lib/autolog.sh
+++ linux/bash/logging/lib/autolog.sh
@@ -22,33 +22,34 @@ start_autolog() {
     local log_file=$CURRENT_LOG/$(basename "$0").log
 
     coproc logproc_writer {
-        cat > "$log_file"
+        exec cat > "$log_file"
     }
     local log_writer_fd=${logproc_writer[1]}
 
     logproc_stdout() {
         set +x
-        while IFS= read -r line; do
-            echo "$line" >&$log_writer_fd
-            echo "$line" >&3
-        done
+        exec tee -a /dev/fd/${AUTOLOG_STDOUT} >&$log_writer_fd
     }
 
     logproc_stderr() {
         set +x
-        while IFS= read -r line || [ -n "$line" ]; do
-            echo "stderr: $line" >&$log_writer_fd
-            if [ -t 4 ]; then
-                term_color $TERM_AF_RED
-                echo "$line"
-                term_clear
-            else
-                echo "stderr: $line"
-            fi >&4
-        done
+        local color_code="$(term_color $TERM_AF_RED)"
+        local color_clear="$(term_clear)"
+        local writer_fd
+        exec {writer_fd}>&$log_writer_fd
+        if [ -t ${AUTOLOG_STDOUT} ]; then
+            exec tee -a >(exec sed -ue "s@^@stderr: @" >&${writer_fd}) | \
+                    sed -uE "s@^(.+)\$@$color_code\\1$color_clear@" >&${AUTOLOG_STDERR}
+        else
+            exec tee -a >(exec sed -ue "s@^@stderr: @" >&${writer_fd}) | \
+                    sed -ue "s@^@stderr: @" >&${AUTOLOG_STDERR}
+        fi
     }
 
-    exec 3>&1 4>&2 > >(logproc_stdout) 2> >(logproc_stderr)
+    exec {AUTOLOG_STDOUT}>&1 {AUTOLOG_STDERR}>&2 > >(logproc_stdout) 2> >(logproc_stderr)
+
+    # trap EXIT to pass the context to the loggers
+    trap 'sleep 0' EXIT
 }
 
 if bool "$ENABLE_AUTOLOG"; then
